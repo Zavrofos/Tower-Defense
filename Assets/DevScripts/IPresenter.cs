99ffbc4955c430dc3210dd0419d6639ea83269c1
Rebrin <rebrin.na@gmail.com>
2023-11-04 18:11:59 +0600

    Init (Added architecture scripts)

---
+++ Assets/DevScripts/IPresenter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.DevScripts
+{
+    public interface IPresenter 
+    {
+        void Subscribe();
+        void Unsubscribe();
+    }
+}
