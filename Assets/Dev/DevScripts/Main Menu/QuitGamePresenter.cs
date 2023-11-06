fe43cebf0d0103a540d7ed27c047791f192d681b
Rebrin <rebrin.na@gmail.com>
2023-11-06 16:15:22 +0600

    Created QuitGamePresenter

---
+++ Assets/Dev/DevScripts/Main Menu/QuitGamePresenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Dev.DevScripts.Main_Menu
+{
+    public class QuitGamePresenter : IPresenter
+    {
+        public void Subscribe()
+        {
+            
+        }
+
+        public void Unsubscribe()
+        {
+           
+        }
+    }
+}
