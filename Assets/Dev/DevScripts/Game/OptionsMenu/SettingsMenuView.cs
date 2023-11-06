6edbfb26c076137f881d02d61889cc3c44f50676
Rebrin <rebrin.na@gmail.com>
2023-11-06 20:48:40 +0600

    Created Options Menu View

---
+++ Assets/Dev/DevScripts/Game/OptionsMenu/SettingsMenuView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenuView : MonoBehaviour
+{
+    public TMP_Dropdown DropDownGraphics;
+    public TMP_Dropdown DropdownResolutions;
+    public Toggle ToggleFullscreen;
+    public Slider VolumeMusic;
+    public Slider VolumeGame;
+    public Button CloseSettingsWindow;
+}
