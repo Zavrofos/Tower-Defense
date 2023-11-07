3ebd851dfd5a1099f2f01e02387c65430aca3a07
Rebrin <rebrin.na@gmail.com>
2023-11-07 14:50:26 +0600

    Added GameManagerLevel and LevelView

---
+++ Assets/Dev/DevScripts/Levels/GameManagerLevel.cs
@@ -0,0 +1,51 @@
+using Assets.Dev.DevScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerLevel : MonoBehaviour
+{
+    [HideInInspector] public GameModel Model;
+    public LevelView View;
+    public List<IPresenter> Presenters;
+    public List<IUpdatable> Updaters;
+
+    private void Awake()
+    {
+        Model = GameManagerDev.Instance.Model;
+
+        Presenters = new()
+        {
+
+        };
+
+        Updaters = new()
+        {
+
+        };
+    }
+
+    private void Update()
+    {
+        foreach(var updater in Updaters)
+        {
+            updater.Update(Time.deltaTime);
+        }
+    }
+
+    private void OnEnable()
+    {
+        foreach(var presenter in Presenters)
+        {
+            presenter.Subscribe();
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var presenter in Presenters)
+        {
+            presenter.Unsubscribe();
+        }
+    }
+}
---
+++ Assets/Dev/DevScripts/Levels/LevelView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Dev.DevScripts.Levels
+{
+    public class LevelView : MonoBehaviour
+    {
+        
+    }
+}
