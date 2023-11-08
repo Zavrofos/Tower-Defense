865a1192a9e74de1948e0ac16c9a20fa109fe56a
Rebrin <rebrin.na@gmail.com>
2023-11-09 00:53:30 +0600

    Added OpenSettingsMenuPresenter

---
+++ Assets/Dev/DevScripts/Game/SettingsMenu/OpenSettingsMenuPresenter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Dev.DevScripts.Game.SettingsMenu
+{
+    public class OpenSettingsMenuPresenter : IPresenter
+    {
+        private GameViewDev _view;
+        private GameModel _model;
+
+        public OpenSettingsMenuPresenter(GameViewDev view, GameModel model)
+        {
+            _view = view;
+            _model = model;
+        }
+
+        public void Subscribe()
+        {
+            _model.SettingsModel.OpenedSettingMenu += OpenSettingsMenu;
+        }
+
+        public void Unsubscribe()
+        {
+            _model.SettingsModel.OpenedSettingMenu -= OpenSettingsMenu;
+        }
+
+        private void OpenSettingsMenu()
+        {
+            if(_model.CurrentStateGame == StateGame.OnPause)
+            {
+                _view.PauseMenuView.PouseWindow.SetActive(false);
+            }
+            _view.SettingsMenuView.gameObject.SetActive(true);
+        }
+    }
+}
