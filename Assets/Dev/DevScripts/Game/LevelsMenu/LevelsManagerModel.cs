18116bc2cb4c4e38b61175669eeceb4232ad83b2
Rebrin <rebrin.na@gmail.com>
2023-11-09 20:13:53 +0600

    Refactoring LevelsMenuView

--- Assets/Dev/DevScripts/Game/LevelsMenu/LevelsManagerModel.cs
+++ Assets/Dev/DevScripts/Game/LevelsMenu/LevelsManagerModel.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using Assets.Dev.DevScripts.Main_Menu.LevelsMenu;
 
 namespace Dev.DevScripts.Game.LevelsMenu
 {
     public class LevelsManagerModel 
     {
-        public List<LevelDev> Levels;
+        public readonly List<LevelModel> Levels = new();
         public event Action OpenedLevelsMenu;
         public event Action ClosedLevelsMenu;
+        public event Action<LevelModel> AddedLevel;
 
-        public LevelsManagerModel()
-        {
-            Levels = new List<LevelDev>();
-        }
-        
         public void OpenLevelsMenu()
         {
             OpenedLevelsMenu?.Invoke();
@@ -23,6 +18,11 @@ namespace Dev.DevScripts.Game.LevelsMenu
         public void CloseLevelsMenu()
         {
             ClosedLevelsMenu?.Invoke();
+        }
+
+        public void AddLevel(LevelModel level)
+        {
+            AddedLevel?.Invoke(level);
         }
     }
 }
