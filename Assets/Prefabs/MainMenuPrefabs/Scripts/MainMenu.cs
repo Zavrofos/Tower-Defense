5127cfe1b0ee2011624da8addd8d49ac53123dc3
ÐZavrofos <rebrin.na98@gmail.com>
2026-02-17 21:08:42 +0500

    update

--- Assets/Prefabs/MainMenuPrefabs/Scripts/MainMenu.cs
+++ Assets/Prefabs/MainMenuPrefabs/Scripts/MainMenu.cs
@@ -1,4 +1,8 @@
+using System;
+using Cysharp.Threading.Tasks;
+using SaveSystemDir;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -8,6 +12,15 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button _settingsButton;
     [SerializeField] private Button _quitButton;
     [SerializeField] private SettingsMenu _settingsMenu;
+    [SerializeField] private AudioMixer _audioMixer;
+
+    private async UniTask Awake()
+    {
+        await UniTask.Yield();
+        
+        _audioMixer.SetFloat("MusicVolume", FormatToDb(SaveSystem.GetVolumeMusic()));
+        _audioMixer.SetFloat("GameVolume", FormatToDb(SaveSystem.GetVolumeGame()));
+    }
 
     private void OnPlay()
     {
@@ -36,6 +49,12 @@ public class MainMenu : MonoBehaviour
         _playButton.onClick.RemoveListener(OnPlay);
         _settingsButton.onClick.RemoveListener(OnOpenOptions);
         _quitButton.onClick.RemoveListener(OnQuit);
+    }
+    
+    private float FormatToDb(float value01)
+    {
+        value01 = Mathf.Clamp(value01, 0.0001f, 1f);
+        return Mathf.Log10(value01) * 20f;
     }
 
 }
